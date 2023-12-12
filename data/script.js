dc0f69b84856c087a4225cbc085fec5a4b7a3ab3
Oleg <r96_23@list.ru>
2023-12-12 13:24:15 +0500

    'modified files .html, .js'

--- data/script.js
+++ data/script.js
@@ -1,28 +1,34 @@
 'use strict'
 
- fetch('https://raw.githubusercontent.com/Bodrbli/Collections/main/data/index.json')
+fetch('https://raw.githubusercontent.com/Bodrbli/Collections/main/data/index.json')
       .then(response => response.json())
       .then(json => json.forEach(render))
 
       function render(value) {
-                  const bodyRow = document.querySelector('tbody');
-                  const t = document.querySelector('#element-row');
-                  bodyRow.append(t.content.cloneNode(true));
 
-                  const columnFillCollecrion = t.content.querySelector('#fillCollection');
-                        columnFillCollecrion.textContent = values[j],[0];
-                  const columnFirst5Percent = t.content.querySelector('#first5Percent');
-                        columnFirst5Percent.textContent = values[j],[1];
-                  const columnLast5Percent = t.content.querySelector('#last5Percent');
-                        columnLast5Percent.textContent = values[j],[2];
-                  const columnRemoveFirst = t.content.querySelector('#removeFirst');
-                        columnRemoveFirst.textContent = values[j],[3];
-                  const columnRemoveLast = t.content.querySelector('#removeLast');
-                        columnRemoveLast.textContent = values[j],[4];
-                  const columnSetName = t.content.querySelector('#setName');
-                        columnSetName.textContent = values[j],[5];
-                  const columnRemoveName = t.content.querySelector('#removeName');
-                        columnRemoveName.textContent = values[j],[6];
-            }
+
+            const bodyRow = document.querySelector('tbody');
+            const t = document.querySelector('#element-row');
+
+
+            const columnType = t.content.querySelector('#type');
+                  columnType.textContent = value.ListName;
+            const columnCount = t.content.querySelector('#count');
+                  columnCount.textContent = value.Num;
+            const columnFillCollection = t.content.querySelector('#fillCollection');
+                  columnFillCollection.textContent = value.FillCollection;
+            const columnFirst5Percent = t.content.querySelector('#first5Percent');
+                  columnFirst5Percent.textContent = value.First5Percent;
+            const columnLast5Percent = t.content.querySelector('#last5Percent');
+                  columnLast5Percent.textContent = value.Last5Percent;
+            const columnRemoveFirst = t.content.querySelector('#removeFirst');
+                  columnRemoveFirst.textContent = value.RemoveFirst;
+            const columnRemoveLast = t.content.querySelector('#removeLast');
+                  columnRemoveLast.textContent = value.RemoveLast;
+            const columnSetName = t.content.querySelector('#setName');
+                  columnSetName.textContent = value.SetName;
+            const columnRemoveName = t.content.querySelector('#removeName');
+                  columnRemoveName.textContent = value.RemoveName;
+
+            bodyRow.append(t.content.cloneNode(true));
       }
-      showTable();
